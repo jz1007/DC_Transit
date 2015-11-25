4ae1ad78194f530e9d48e019c653f0f70e73e4cd
numberone107 <jzavilla+code@alumni.nd.edu>
2015-11-24 23:41:29 -0500

    Update wmata.py

--- wmata.py
+++ wmata.py
@@ -15,8 +15,9 @@ def query(station):
         data = response.read()
         conn.close()
     except Exception as e:
-        print "Oh noes" + str(e)
+        print "Oh noz" + str(e)
     print "Got some data! Returning it now."
+    print ""
     return json.loads(data)
 
 response = query("B03")
@@ -24,14 +25,11 @@ response = query("B03")
 trains = response['Trains']
 
 for train in trains:
-#    print json.dumps(train)
-    print train['LocationName']
-    print train['DestinationName']
-    print train['Min']
-	
-#if response['Group'] == '1':
-#	print "To Glenmont"
-#	print train['Min']
-#else
-#	print "To Shady Grove"
-#	print train['Min']
+	#print json.dumps(train)
+	if train['Group'] == '1':
+		#print train['LocationName'] + " To Glenmont via Noma"
+		print train['DestinationName'] + " " + train['Min']
+		print ""
+	if train['Group'] == '2':
+		#print train['LocationName'] + " To Shady Grove via Metro Center"
+		print train['DestinationName'] + " " + train['Min']
