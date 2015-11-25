3f9e4b948955e01b50fb79ee2fcc77b908768092
numberone107 <jzavilla+code@alumni.nd.edu>
2015-11-24 20:24:13 -0500

    Update wmata_api_call.py

--- wmata_api_call.py
+++ wmata_api_call.py
@@ -11,7 +11,6 @@ params = urllib.urlencode({
 try:
     conn = httplib.HTTPSConnection('api.wmata.com')
     conn.request("GET", "/StationPrediction.svc/json/GetPrediction/B03", "{body}", headers)
-    conn.request("GET", "/StationPrediction.svc/json/GetPrediction/B04", "{body}", headers)
     response = conn.getresponse()
     data = response.read()
     print(data)
