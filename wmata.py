94a360cf1fa2a074267972ad1906fa067eb27630
numberone107 <jzavilla+code@alumni.nd.edu>
2015-12-05 21:03:31 -0500

    Adding alerts / incident reports
    got lazy on the API key, so change that up later

--- wmata.py
+++ wmata.py
@@ -8,6 +8,8 @@ headers = {
     'api_key': '919aca6de37440c8b9783f96d62f70e8',
 }
 
+########TIME TO TRAIN SET-UP#########
+
 def query(station):
     url = "/StationPrediction.svc/json/GetPrediction/{0}".format(station)
     print "Querying " + url
@@ -27,7 +29,30 @@ response = query("B03")
 
 trains = response['Trains']
 
-def f():
+######END TIME TO TRAIN SET-UP / BEGIN ALERT SET-UP#######
+
+def query(alerts):
+	url = "/Incidents.svc/json/Incidents?api_key=919aca6de37440c8b9783f96d62f70e8" ##got lazy on API key
+	print "Querying " + url
+	try:
+		conn = httplib.HTTPSConnection('api.wmata.com')
+		conn.request("GET", url)
+		response = conn.getresponse()
+		data = response.read()
+		conn.close()
+	except Exception as e:
+		print "Oh noz" + str(e)
+	print "Returning data now..."
+	print ""
+	return json.loads(data)
+
+response2 = query("Incidents") 
+
+alerts = response2['Incidents']
+
+#######END ALERT SET-UP########
+
+def minutes():
 	os.system("clear")
 	times = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S') 
 	print ""    
@@ -40,12 +65,19 @@ def f():
 		#print json.dumps(train)
 		if train['Group'] == '1':
 			#print train['LocationName'] + " To Glenmont via Noma"
+			#####add in a 'RUN!!!!' print
 			print "To Glenmont " + train['Min']				
 		
 		if train['Group'] == '2':
 			#print train['LocationName'] + " To Shady Grove via Metro Center"
 			print "To Shady Grove " + train['Min']
 		print ""
-	threading.Timer(5, f).start()
+
+	print ""
+	print "***Alerts***"
+	for alert in alerts:
+		#print json.dumps(alert)
+		print alert['Description']
+	threading.Timer(5, minutes).start()
 	
-f()
+minutes()
