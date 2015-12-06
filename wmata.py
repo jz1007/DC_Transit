e16c49e3297955d1e4c3cc7e0112a3ad676b5ba8
numberone107 <jzavilla+code@alumni.nd.edu>
2015-12-06 00:58:49 -0500

    Update wmata.py

--- wmata.py
+++ wmata.py
@@ -76,7 +76,8 @@ def f():
 	print "***Alerts***"
 	for alert in alerts:
 		#print json.dumps(alert)
-		print "*" + alert['Description']
+		print alert['Description']
+		print ""
 	threading.Timer(5, f).start()
 	
 f()
